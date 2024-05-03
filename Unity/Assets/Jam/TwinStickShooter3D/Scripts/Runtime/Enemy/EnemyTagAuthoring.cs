26989d0c6d26012d66c7f62e66db7f38c004e665
VerticalLongboard <73481831+VerticalLongboard@users.noreply.github.com>
2024-05-03 15:37:25 -0500

    Moved Enemy files to separate folder, Added Enemy tag

---
+++ Unity/Assets/Jam/TwinStickShooter3D/Scripts/Runtime/Enemy/EnemyTagAuthoring.cs
@@ -0,0 +1,18 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace RMC.DOTS.Samples.Games.TwinStickShooter3D
+{
+	public class EnemyTagAuthoring : MonoBehaviour
+    {
+        public class EnemyTagAuthoringBaker : Baker<EnemyTagAuthoring>
+        {
+            public override void Bake(EnemyTagAuthoring tagAuthoring)
+            {
+                Entity entity = GetEntity(TransformUsageFlags.Dynamic);
+                                
+                AddComponent<BulletTag>(entity);
+            }
+        }
+    }
+}
